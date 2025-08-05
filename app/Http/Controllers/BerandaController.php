3f030f96b58333c1acd0bd98e9015a1f26fcf268
zahra10fitri <zahra10fitri@gmail.com>
2025-08-05 08:57:11 +0700

    upload tambahan project

--- app/Http/Controllers/BerandaController.php
+++ app/Http/Controllers/BerandaController.php
@@ -6,10 +6,8 @@ use Illuminate\Http\Request;
 
 class BerandaController extends Controller
 {
-    //
-     public function index()
+    public function index()
     {
         return view('beranda');
     }
-
 }
