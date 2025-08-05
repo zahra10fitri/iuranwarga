cc8b0f456ce2055982865812d470d65916e094ff
zahra10fitri <zahra10fitri@gmail.com>
2025-08-05 08:52:45 +0700

    upload tambahan project

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,4 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BerandaController;
 
-Route::get('/', function () {
-    return view('welcome');
-
 Route::get('/beranda', [BerandaController::class, 'index'])->name('beranda');
-
-});
