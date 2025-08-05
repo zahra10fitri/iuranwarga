08675daed3e43cdecd673b3942d24195a59bd632
zahra10fitri <zahra10fitri@gmail.com>
2025-08-05 12:28:26 +0700

    upload tambahan project

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -12,11 +12,15 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+           $table->id();
             $table->string('name');
+            $table->string('username')->unique(); // ✅ Tambah username
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('nohp');              // ✅ Tambah nohp
+            $table->text('address');             // ✅ Tambah alamat
+            $table->enum('level', ['warga', 'admin']); // ✅ Tambah level
             $table->rememberToken();
             $table->timestamps();
         });
