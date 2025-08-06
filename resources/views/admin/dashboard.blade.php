03dad9a2c141b0140c56e71dd87f15a70f7e94f6
zahra10fitri <zahra10fitri@gmail.com>
2025-08-06 11:12:02 +0700

    tambahan project

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,4 +1,4 @@
-@extends('template.admin')
+@extends('admin.template')
 
 @section('content')
 <div class="container mt-4">
@@ -10,15 +10,15 @@
             <div class="card text-white bg-success mb-3">
                 <div class="card-body d-flex justify-content-between align-items-center">
                     <div>
-                        <h3 class="card-title font-weight-bold">15</h3>
+                        <h3 class="card-title font-weight-bold">{{ $totalWarga }}</h3>
                         <p class="card-text">Data Warga</p>
                     </div>
                     <i class="fas fa-user fa-3x opacity-50"></i>
                 </div>
                 <div class="card-footer bg-transparent border-top-0">
-                    <a href="{{ route('warga.index') }}" class="text-white">
+                    <span class="text-white" style="cursor: not-allowed;">
                         Lihat Data <i class="fas fa-arrow-circle-right"></i>
-                    </a>
+                    </span>
                 </div>
             </div>
         </div>
@@ -28,15 +28,15 @@
             <div class="card text-white bg-primary mb-3">
                 <div class="card-body d-flex justify-content-between align-items-center">
                     <div>
-                        <h3 class="card-title font-weight-bold">6</h3>
+                        <h3 class="card-title font-weight-bold">{{ $totalIuran }}</h3>
                         <p class="card-text">Iuran Kas</p>
                     </div>
                     <i class="fas fa-cash-register fa-3x opacity-50"></i>
                 </div>
                 <div class="card-footer bg-transparent border-top-0">
-                    <a href="{{ route('iuran.index') }}" class="text-white">
+                    <span class="text-white" style="cursor: not-allowed;">
                         Lihat Data <i class="fas fa-arrow-circle-right"></i>
-                    </a>
+                    </span>
                 </div>
             </div>
         </div>
